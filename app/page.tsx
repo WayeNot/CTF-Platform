b785434b2faabe78f03c9cab217a17af64d13695
timeoblrynov <timeo.baffreauleroux@ynov.com>
2026-04-15 15:12:12 +0200

    add : .env

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Navbar from "../components/Navbar.tsx";
 
 export default function Home() {
     return (
         <div>
+            <Navbar/>
             <SpeedInsights />
             <h2 className="text-white/70 text-xl sm:text-7xl mt-100 font-mono text-center">Coming soon ...</h2>
             <a href="/ctf/phishout" className="text-white/70 hover:text-white/40 cursor-pointer transition duration-500 text-xl sm:text-2xl mt-10 font-mono flex justify-center items-center">Return to the phishout challenge</a>
