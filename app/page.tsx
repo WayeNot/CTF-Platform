6cc3aa0fa330aff977421241b8cd12ad72d3b5d7
WayeNot <aymericbeaune917@gmail.com>
2026-05-10 22:04:39 +0200

    [ :bug: ] - Fix beug page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             <p className="text-white/50 font-mono text-[30px] mx-60 mt-5">FlagCore is a free CTF platform dedicated to cybersecurity enthusiasts wishing to develop their skills in a fun and concrete way. Through a series of immersive and progressive challenges, users are immersed in realistic scenarios where they play real cybersecurity investigators.</p>
         
             <div className="flex items-center justify-center mb-20 mt-20">
-                 <Link href="/login" className="text-white/70 p-3 text-center text-xl text-[30px] font-mono border-t-2 border-b-2 border-white/40 font-bold transition duration-500 hover:bg-white/70 hover:text-[#1e1e2f]">Enter on Flagcore</Link>   
+                 <Link href="/accounts/login" className="text-white/70 p-3 text-center text-xl text-[30px] font-mono border-t-2 border-b-2 border-white/40 font-bold transition duration-500 hover:bg-white/70 hover:text-[#1e1e2f]">Enter on Flagcore</Link>   
             </div>
         </div>
     )
