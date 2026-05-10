469f76ec2ed0d4c9f19e3dfd0d2db21b7ff9f696
timeoblrynov <timeo.baffreauleroux@ynov.com>
2026-05-10 22:11:51 +0200

    bug fixe

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -74,36 +74,6 @@ export default function Navbar() {
                 </div>
             )}
             {!isGuest && (
-                <div>
-                    {warn && warn.show_notif && warn.reason && (
-                        <p onClick={() => setShowWarn(true)} className="flex items-center justify-center gap-3 text-white/40 p-4 rounded-lg w-full border border-orange-600 text-[20px] text-center cursor-pointer hover:text-white/20 transition duration-500"><IoWarning className="text-orange-500" />Vous avez actuellement un avertissement : <span className="text-orange-500">{warn?.reason}</span><IoWarning className="text-orange-500" /></p>
-                    )}
-                    <nav className="flex items-center justify-between p-4 sm:mx-5">
-                        <div className="flex items-center">
-                            <Link href="/home" className="text-xl h-fit sm:text-2xl text-white/60 font-mono mr-12">FlagCore</Link>
-                            <div className="hidden sm:flex items-center gap-5 text-white/40">
-                                <Link href="/home" className="hover:text-white/70 hover:text-underline border-2 border-[#212529] hover:border-t-2 hover:border-b-2 hover:border-t-white hover:border-b-white pt-1 pb-1 transition duration-500 font-mono text-[20px]">Accueil</Link>
-                                <Link href="/tools" className="hover:text-white/70 border-2 border-[#212529] hover:border-t-2 hover:border-b-2 hover:border-t-white hover:border-b-white pt-1 pb-1 transition duration-500 font-mono ml-5 text-[20px]">Tools</Link>
-                                <Link href="/challenges" className="hover:text-white/70 border-2 border-[#212529] hover:border-t-2 hover:border-b-2 hover:border-t-white hover:border-b-white pt-1 pb-1 transition duration-500 font-mono ml-5 mr-5 text-[20px]">Nos challenges</Link>
-                            </div>
-                        </div>
-                        <button onClick={() => setMenuOpen(!menuOpen)} className="sm:hidden text-white text-2xl">☰</button>
-                        <div className="hidden sm:flex items-center text-white/40">
-                            <div className="flex items-center gap-5 font-bold text-white/40 mr-6">
-                                <p className="flex items-center gap-3 text-[18px] hover:text-white/70 transition font-mono duration-500"><img src={pp_url || default_pp} alt="Logo de l'utilisateur" className={`w-10 bg-center bg-cover bg-no-repeat ${statusColor[status ?? "offline"]}`} /><span className="mx-2">-</span>{username}</p>
-                            </div>
-                            <p className="flex items-center gap-3 text-white/70 text-[20px] transition duration-500 ml-5"><RiCoinsFill />{coins}</p>
-                            <p className="text-white/70 text-[20px] mx-5"> | </p>
-                            <p className="flex items-center gap-3 text-white/70 text-[20px] transition duration-500 mr-15"><SiOpslevel />{points}</p>
-                            <MdExitToApp onClick={handleLogout} className="hover:text-red-400 cursor-pointer text-2xl transition duration-500" />
-                            <div className="flex ml-3 items-center gap-5 text-white/40">
-                                {Array.isArray(permissions) && (permissions.includes(Permissions.advanced.administrator) || permissions.includes(Permissions.panelAdmin.canOpen)) && <div className="flex items-center gap-3"><MdAdminPanelSettings onClick={() => setShowAdminPanel(true)} className="font-bold text-[30px] hover:text-red-800 transition duration-500 cursor-pointer ml-7" /></div>}
-                            </div>
-                        </div>
-                    </nav>
-                </div >
-            )}
-            {!isGuest && (
                     <div>
                         {warn && warn.show_notif && warn.reason && (
                             <p onClick={() => setShowWarn(true)} className="flex items-center justify-center gap-3 text-white/40 p-4 rounded-lg w-full border border-orange-600 text-[20px] text-center cursor-pointer hover:text-white/20 transition duration-500"><IoWarning className="text-orange-500" />Vous avez actuellement un avertissement : <span className="text-orange-500">{warn?.reason}</span><IoWarning className="text-orange-500" /></p>
